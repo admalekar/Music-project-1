5f2b11d4ea25f2f244b4be40d6f57a0a4db0fa7b
Amey Malekar <admalekar@gmail.com>
2018-12-25 21:16:38 +0530

    sql database 12/25/2018

---
+++ music.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 25, 2018 at 04:45 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `music`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_details`
+--
+
+CREATE TABLE IF NOT EXISTS `student_details` (
+  `student_id` int(4) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Date_of_birth` varchar(10) NOT NULL,
+  `Batch` int(2) NOT NULL,
+  `Start_date` varchar(10) NOT NULL,
+  `Contact` int(10) NOT NULL,
+  `Parent_name` varchar(20) NOT NULL,
+  `Parent_contact` int(10) NOT NULL,
+  `Mother_tongue` varchar(10) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Exam_level` int(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_details`
+--
+
+INSERT INTO `student_details` (`student_id`, `Name`, `Date_of_birth`, `Batch`, `Start_date`, `Contact`, `Parent_name`, `Parent_contact`, `Mother_tongue`, `Address`, `Exam_level`) VALUES
+(1, 'Amey', '17/12/18', 1, '17/12/18', 875454545, 'sdfsf', 787878777, 'Marath', 'adfafd', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `userid` int(4) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `username`, `password`, `admin`) VALUES
+(1001, 'admin', '123', 1),
+(1002, 'amey', '12345', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_details`
+--
+ALTER TABLE `student_details`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_details`
+--
+ALTER TABLE `student_details`
+  MODIFY `student_id` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
